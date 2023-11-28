93c0d0033fbd513da525155437e32de85992288e
Huy Pham <huypham8501@gmail.com>
2023-11-28 23:37:36 +0700

    handle app doesn't have permission using BLE

--- ClassFaceAttendance/ViewController/MainViewController.swift
+++ ClassFaceAttendance/ViewController/MainViewController.swift
@@ -36,11 +36,11 @@ class MainViewController: BaseViewController {
         firebaseManager.checkUserRole { [weak self] role in
             guard let role = role else {
                 self?.showAlertViewController(title: "Tài khoản đã bị vô hiệu hoá hoặc đã có lỗi xảy ra",
-                                        actions: [],
-                                        cancel: "OK",
-                                        cancelHandler: { [weak self] in
-                                            self?.dismiss(animated: true)
-                                        })
+                                              actions: [],
+                                              cancel: "OK",
+                                              cancelHandler: { [weak self] in
+                                                  self?.dismiss(animated: true)
+                                              })
                 return
             }
             switch role {
@@ -70,11 +70,25 @@ extension MainViewController: CBPeripheralManagerDelegate {
             ])
             firebaseManager.uploadTeacherDeviceName(deviceName: globalUser?.id ?? "", teacherId: globalUser?.id ?? "")
         }
+        if peripheral.state == .unauthorized {
+            showAlertViewController(title: "Không có quyền sử dụng Bluetooth",
+                                    message: "Vui lòng cho phép sử dụng Bluetooth trong cài đặt của bạn",
+                                    actions: ["Cài đặt"],
+                                    cancel: nil) { index in
+                if index == 0 {
+                    if let bundleId = Bundle.main.bundleIdentifier,
+                       let url = URL(string: "\(UIApplication.openSettingsURLString)&root=Privacy&path=Bluetooth/\(bundleId)") {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+            }
+        }
     }
 
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
         if error == nil {
             print("Peripheral is advertising")
+            showToast(message: "Đang phát tín hiệu bluetooth!")
         } else {
             print("Error advertising peripheral: \(error!.localizedDescription)")
         }
